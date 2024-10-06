69231b9ff8903cfb3b44f55614d79610086c95e5
Alexandra Kufilova <alexahariwer@gmail.com>
2024-10-06 21:34:03 +0300

    Added function to_float_hours

--- extra-task-1.h
+++ extra-task-1.h
@@ -10,5 +10,12 @@ double seconds_difference(double time_1, double time_2);
 //time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2);
 
+/*
+    Return the total number of hours in the specified number
+    of hours, minutes, and seconds.
+*/
+double to_float_hours(int hours, int minutes, int seconds);
+
+
 
 #endif // !EXTRA_TASK_H
