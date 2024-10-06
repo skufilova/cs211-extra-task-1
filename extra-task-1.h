404cdc59b8117e919fb983a75b492db17ebb3086
Alexandra Kufilova <alexahariwer@gmail.com>
2024-10-06 21:21:58 +0300

    Added function hours_difference

--- extra-task-1.h
+++ extra-task-1.h
@@ -6,6 +6,9 @@
 // time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2);
 
+//Return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1.
+double hours_difference(double time_1, double time_2);
 
 
 #endif // !EXTRA_TASK_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "extra-task-1.h"
 #include <iostream>
 #include <cassert>
-
 using namespace std;
 
 int main()
@@ -11,4 +10,12 @@ int main()
 	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
 	assert(seconds_difference(1800.0, 2160.0) == 360.0);
 	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+
+	//function hours_difference
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
+
+	return 0;
 }
