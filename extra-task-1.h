7602dc7fbe5018529864a017ade3a77ed3c29d11
Alexandra Kufilova <alexahariwer@gmail.com>
2024-10-06 20:55:20 +0300

    Added function seconds_difference

---
+++ extra-task-1.h
@@ -0,0 +1,11 @@
+#ifndef EXTRA_TASK_H
+#define EXTRA_TASK_H
+
+
+// Return the number of seconds later that a time in seconds
+// time_2 is than a time in seconds time_1.
+double seconds_difference(double time_1, double time_2);
+
+
+
+#endif // !EXTRA_TASK_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "extra-task-1.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+	//function seconds_difference
+	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+	assert(seconds_difference(1800.0, 2160.0) == 360.0);
+	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+}
